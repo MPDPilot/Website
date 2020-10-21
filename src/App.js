4629afb4fd2383e1b3892dffca29b9b62555e13e
Noemi Rozpara <nrozpara@gmail.com>
2020-10-21 18:32:20 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Grid,
+  Typography,
+  List,
+  ListItemIcon,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container justify="center" spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h1">
+            MPDPilot / Manage your MPD flying high!
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h1" component="p">
+            Available on Appstore
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container justify="center" spacing={2}>
+        <Grid item xs={12} sm={3}>
+          <Typography variant="h3">V1.0.0</Typography>
+        </Grid>
+        <Grid item xs={12} sm={9}>
+          <List component="nav" aria-label="main mailbox folders">
+            <ListItem>
+              <ListItemText primary="Inbox" />
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
+      <Box flexDirection="row">
+        Features
+        <Box>V1.0.0</Box>
+        <Box>A/B/C</Box>
+      </Box>
+      <Box>Gallery</Box>
+      <Box>
+        Maintainers
+        <Box>Noemi</Box>
+        <Box>Piotr</Box>
+      </Box>
+      <Box>Contact</Box>
+      <Box>Privacy policy</Box>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,10 @@
+const data = {
+  versions: [
+    {
+      number: "1.0.0",
+      features: ["a", "b", "c"],
+    },
+  ],
+};
+
+export default data;
