0a63719eb7fd979046bdf400678353961b2a0f0f
Noemi Rozpara <nrozpara@gmail.com>
2020-10-22 20:15:30 +0200

    feat: build

---
+++ docs/precache-manifest.ab8ec6fc1ce28a81d30886542f5028b8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b71e80972cc8daca2c3c222655b615",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13b667a22ed5465a0b7b",
+    "url": "/static/js/2.cce42362.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/static/js/2.cce42362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b40f585a1d243eab4f",
+    "url": "/static/js/main.c0cce184.chunk.js"
+  },
+  {
+    "revision": "53911609e47d307166fc",
+    "url": "/static/js/runtime-main.00abe04e.js"
+  },
+  {
+    "revision": "0f3514a45d51f95167e5fe8b6a03bb60",
+    "url": "/static/media/appStoreBadge.0f3514a4.svg"
+  },
+  {
+    "revision": "5dbc77f02f8e9818a53d2e4932edfc24",
+    "url": "/static/media/s1.5dbc77f0.png"
+  },
+  {
+    "revision": "7c07eea0b9b74bc258e44f6133b02360",
+    "url": "/static/media/s2.7c07eea0.png"
+  },
+  {
+    "revision": "4b6a4566427f00fd39443bb7dffebe79",
+    "url": "/static/media/s3.4b6a4566.png"
+  },
+  {
+    "revision": "38922c0acf3048d35463be903fc128ec",
+    "url": "/static/media/s4.38922c0a.png"
+  },
+  {
+    "revision": "f0a1beb6fab9b360c398d818d755e87d",
+    "url": "/static/media/s5.f0a1beb6.png"
+  },
+  {
+    "revision": "fe15c97685aeb01009eed00160925fc2",
+    "url": "/static/media/s6.fe15c976.png"
+  }
+]);
