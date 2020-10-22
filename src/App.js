1c3fa15630920f3fdb4b55919e14e41153ddb17b
Noemi Rozpara <nrozpara@gmail.com>
2020-10-22 12:29:10 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,57 +1,116 @@
+import React from "react";
+import mpdPilotLogoTransparent from "./assets/mpdPilotLogoTransparent.png";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
 import {
   Box,
-  Grid,
-  Typography,
   List,
-  ListItemIcon,
   ListItem,
   ListItemText,
+  ThemeProvider,
 } from "@material-ui/core";
-import React from "react";
-import "./App.css";
+import Hero from "./sections/Hero";
+import Footer from "./sections/Footer";
+import theme from "./theme";
+
+const cards = [1, 2, 3];
+
+const tileData = [
+  {
+    img: mpdPilotLogoTransparent,
+    title: "Image",
+    author: "author",
+  },
+  {
+    //  [etc...]
+  },
+];
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <Grid container justify="center" spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h1">
-            MPDPilot / Manage your MPD flying high!
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h1" component="p">
-            Available on Appstore
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid container justify="center" spacing={2}>
-        <Grid item xs={12} sm={3}>
-          <Typography variant="h3">V1.0.0</Typography>
-        </Grid>
-        <Grid item xs={12} sm={9}>
-          <List component="nav" aria-label="main mailbox folders">
-            <ListItem>
-              <ListItemText primary="Inbox" />
-            </ListItem>
-          </List>
-        </Grid>
-      </Grid>
-      <Box flexDirection="row">
-        Features
-        <Box>V1.0.0</Box>
-        <Box>A/B/C</Box>
-      </Box>
-      <Box>Gallery</Box>
-      <Box>
-        Maintainers
-        <Box>Noemi</Box>
-        <Box>Piotr</Box>
-      </Box>
-      <Box>Contact</Box>
-      <Box>Privacy policy</Box>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <main>
+        <Hero />
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            <GridList className={classes.gridList} cols={2.5}>
+              {tileData.map((tile) => (
+                <GridListTile key={tile.img}>
+                  <img src={tile.img} alt={tile.title} />
+                  <GridListTileBar
+                    title={tile.title}
+                    classes={{
+                      root: classes.titleBar,
+                      title: classes.title,
+                    }}
+                  />
+                </GridListTile>
+              ))}
+            </GridList>
+          </Grid>
+        </Container>
+        <Container>
+          Features
+          <Grid item xs={12} sm={9}>
+            <List component="nav" aria-label="main mailbox folders">
+              <ListItem>
+                <ListItemText primary="Inbox" />
+              </ListItem>
+            </List>
+          </Grid>
+        </Container>
+        <Container>Gallery</Container>
+        <Container>Maintainers</Container>
+        <Container>Contact</Container>
+      </main>
+      <Footer />
+    </ThemeProvider>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: "rgb(13,113,254)",
+    background:
+      "linear-gradient(0deg, rgba(13,113,254,1) 0%, rgba(88,168,255,1) 100%)",
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 
 export default App;
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+
+import mpdPilotLogoTransparent from "../assets/mpdPilotLogoTransparent.png";
+import appStoreBadge from "../assets/appStoreBadge.svg";
+
+function Hero() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <div className={classes.heroContent}>
+        <Container maxWidth="sm">
+          <Box
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="center"
+            className={classes.heroRow}
+          >
+            <img
+              src={mpdPilotLogoTransparent}
+              alt="MPD Pilot Logo"
+              className={classes.icon}
+            />
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textSecondary"
+            >
+              MPD Pilot
+            </Typography>
+          </Box>
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            paragraph
+            className={classes.heroRow}
+          >
+            Manage your MPD server flying high!
+          </Typography>
+          <Grid container justify="center">
+            <Grid item>
+              <Link href="#">
+                <img src={appStoreBadge} alt="App Store download button" />
+              </Link>
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+    width: "100px",
+    height: "100px",
+  },
+  heroContent: {
+    backgroundColor: "rgb(13,113,254)",
+    background:
+      "linear-gradient(0deg, rgba(13,113,254,1) 0%, rgba(88,168,255,1) 100%)",
+    padding: theme.spacing(10, 0, 10),
+  },
+  heroRow: {
+    marginBottom: theme.spacing(6),
+  },
+}));
+
+export default Hero;
